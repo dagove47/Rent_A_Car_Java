d17d479b8effd9deccfaf8e6a84ae4d5ab2ccd46
David Gomez <dagove47@gmail.com>
2022-11-26 19:49:04 -0600

    Structure Update
    Structure Update

---
+++ src/main/java/Rent_A_Car/NodoV.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Rent_A_Car;
+
+/**
+ *
+ * @author David Gomez, Gabriel Brilla, Gerald Blanco, Alexander Torres
+ */
+public class NodoV {
+    
+}
