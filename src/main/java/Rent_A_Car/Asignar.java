7df5f7c58362ea05d159dfd14b0969ac53c9d626
gabrielbrilla <gabriel_brilla@yahoo.com>
2022-12-11 14:18:57 -0600

    Update Asignar.java

--- src/main/java/Rent_A_Car/Asignar.java
+++ src/main/java/Rent_A_Car/Asignar.java
@@ -1,5 +1,12 @@
 package Rent_A_Car;
 public class Asignar {
+    
+    /*
+    * Se asignan todas las variables que se van a usar en esta clase 
+    * y seguidamente se crea un setter y un getter para poder 
+    * determinar el valor y utilizar ese mismo valor desde 
+    */
+    
     private String placa;
     private String cedula;
     private String estadoSolicitud;
@@ -30,13 +37,24 @@ public class Asignar {
     public void setEstadoSolicitud(String estadoSolicitud) {
         this.estadoSolicitud = estadoSolicitud;
     }
-
+    
+    /*
+    * El metodo asignar guarda en si mismo el la placa la cedula 
+    * y el estado de la solicitud para identificar una asigancion
+    * de manera simple y rapida no solo para el usuario
+    * pero tambien para la maquina que desempeña el codigo.
+    */
+    
     public Asignar(String placa, String cedula, String estadoSolicitud) {
         this.placa = placa;
         this.cedula = cedula;
         this.estadoSolicitud = estadoSolicitud;
     }
-    
+    /*
+    * Este override esta aqui para que cuando se pida el print 
+    * de los contenidos de la lista se pudan dar todos los datos de una
+    * manera que sea entendible y utilizable.
+    */
     @Override
     public String toString(){return "Asignar{" + "placa="+placa+", cedula="+cedula+"," +
             " estado="+estadoSolicitud+'}';
