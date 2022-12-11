dc6b0221d3c7eb75cbee0eacc301097d6e49f2bb
gabrielbrilla <gabriel_brilla@yahoo.com>
2022-12-11 14:53:02 -0600

    Update ColaSolicitud.java

--- src/main/java/Rent_A_Car/ColaSolicitud.java
+++ src/main/java/Rent_A_Car/ColaSolicitud.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Rent_A_Car;
 
 /**
@@ -9,12 +5,24 @@ package Rent_A_Car;
  * @author David Gomez, Gabriel Brilla, Gerald Blanco, Alexander Torres
  */
 public class ColaSolicitud {
+    /*
+    * Se declaran las variables que se van a usar en la
+    * clase.
+    */
+    
     private NodoS frente;
     private NodoS ultimo;
 
     public ColaSolicitud() {
     }
-    
+    /*
+    * Encola : 
+    * Se declara un nodo nuevo llamado newNodo
+    * si el nodo frente no existe se crea un  nuevo nodo llamado
+    * frente y otro llamado ultimo. Si el nodo llamado frente ya
+    * existe entonces newNodo se pone detras de ultimo y ultimo 
+    * se iguala a Newnodo
+    */
     public void encola(Solicitud solicitud){
         NodoS newNodo=new NodoS(solicitud);
         if(frente == null){
@@ -25,6 +33,14 @@ public class ColaSolicitud {
             ultimo=newNodo;
         }
     }
+    
+    /*
+    * Prefernecial : 
+    * se crea un nodo nuevo llamado NewNodo, si frente no existe 
+    * se crean dos nodos nuevos uno llamado frente y uno llamado 
+    * ultimo, si frente ya existe entonces newNodo se crea antes que frente
+    * y frente se iguala a newNodo.
+    */
     
     public void preferencial(Solicitud solicitud) {
         NodoS newNodo=new NodoS(solicitud);
@@ -49,6 +65,16 @@ public class ColaSolicitud {
 //        return result;
 //    }
     
+    /*
+    * Atiende : 
+    * Se crea un nuevo Nodo lamado aux que es
+    * exactamente igual que frente, si frente 
+    * existe entonces frente se convierte en 
+    * el nodo que esta antes, aux pierde su
+    * valor y el valor de aux se regresa
+    * como el resultado de atiende.
+    */
+    
     public NodoS atiende(){
         NodoS aux = frente;
         if(frente!=null){
@@ -57,6 +83,18 @@ public class ColaSolicitud {
         }
         return aux;
     }
+    
+    /*
+    * El override convierte la lista en un string 
+    * entonces se crea un sting llamado "s" y un nodo
+    * llamado aux que es igual a frente, luego si aux 
+    * existe el contenido de aux se agrega al vacio
+    * que es "s" y se crea una nueva linea de texto. 
+    * Luego aux se convierte en el nodo que existe 
+    * detras del frente y luego se da como resultado
+    * "s"
+    */
+    
     
     @Override
     public String toString(){
