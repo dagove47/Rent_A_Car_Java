3b2fa3b98e5efe79985c527757816fd2cd4dcbd2
gabrielbrilla <gabriel_brilla@yahoo.com>
2022-12-12 12:43:05 -0600

    Update NodoS.java

--- src/main/java/Rent_A_Car/NodoS.java
+++ src/main/java/Rent_A_Car/NodoS.java
@@ -1,19 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Rent_A_Car;
 
 /**
  *
  * @author David Gomez, Gabriel Brilla, Gerald Blanco, Alexander Torres
  */
+
+
 public class NodoS {
+    
+    //<editor-fold defaultstate="collapsed" desc="Variables">
+    /*
+    * Se declaran las variables que 
+    * van a ser usadas en el futuro.
+    */    
+    //</editor-fold>
+    
     private Solicitud dato;
     private NodoS nodoAnterior;
     
     public NodoS() {}
-
+    
+    //<editor-fold defaultstate="collapsed" desc="Setter y Getter">
+    /*
+    * Se crea un setter y un getter
+    * para todos los datos que seran
+    * utilizados en otras clases y en el main.
+    */    
+    //</editor-fold>
+    
     public Solicitud getDato() {
         return dato;
     }
@@ -30,9 +44,27 @@ public class NodoS {
         this.nodoAnterior = nodoAnterior;
     }
     
+    //<editor-fold defaultstate="collapsed" desc="(Solicituyd dato)">
+    /*
+    * Se añade "Solicitud dato" dentro del parentesis de
+    * NodoS para que cuando el codigo se corra
+    * se sepa que se pueda aceptar "dato" siempre
+    * y cuando sea tipo "Solicitud"
+    */    
+    //</editor-fold>
+    
     public NodoS(Solicitud dato) {
         this.dato = dato;
     }
+    
+    //<editor-fold defaultstate="collapsed" desc="toString">
+    /*
+    * Se hace uso del toString de java para 
+    * generar una representacion de dato 
+    * tipo string para poder poner en la
+    * lista.
+    */    
+    //</editor-fold>
     
     @Override
     public String toString(){
