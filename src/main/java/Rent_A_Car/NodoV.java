8faec4c1e8c8d038f76cd4076962446af7b0a16e
gabrielbrilla <gabriel_brilla@yahoo.com>
2022-12-12 12:52:07 -0600

    Update NodoV.java

--- src/main/java/Rent_A_Car/NodoV.java
+++ src/main/java/Rent_A_Car/NodoV.java
@@ -9,18 +9,51 @@ package Rent_A_Car;
  * @author David Gomez, Gabriel Brilla, Gerald Blanco, Alexander Torres
  */
 public class NodoV {
+    
+    //<editor-fold defaultstate="collapsed" desc="Variables">
+    /*
+    * Se crean todas las variables que se van 
+    * en las otras clases y en el main.
+    */    
+    //</editor-fold>
+    
     private Vehiculo dato;
     private NodoV next;
+    
+    //<editor-fold defaultstate="collapsed" desc="toString">
+    /*
+    * convierte el "dato" a un string para 
+    * poder usarlo en la lista y que una 
+    * persona lo pueda leer y enender.
+    */    
+    //</editor-fold>
     
     @Override
     public String toString(){
         return dato.toString();
     }
     
+    //<editor-fold defaultstate="collapsed" desc="(Vehiculo dato)">
+    /*
+    * Se le dice a NodoV que puede
+    * aceptar no solo vacios pero
+    * tambien "dato" siempre y cuando
+    * el "dato" sea de tipo "Vehiculo".
+    */    
+    //</editor-fold>
+    
     public NodoV(Vehiculo dato){
         this.dato=dato;
     }
-
+    
+    //<editor-fold defaultstate="collapsed" desc="Setter y Getter">
+    /*
+    * Se crea un setter y un getter para
+    * todas las varables que se van a utilizar en 
+    * otras clases.
+    */        
+    //</editor-fold>    
+    
     public Vehiculo getDato() {
         return dato;
     }
