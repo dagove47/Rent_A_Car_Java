3d2801a05b4881ce78af2435c7359c15bcb30ca6
gabrielbrilla <gabriel_brilla@yahoo.com>
2022-12-12 12:33:02 -0600

    Update NodoC.java

--- src/main/java/Rent_A_Car/NodoC.java
+++ src/main/java/Rent_A_Car/NodoC.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Rent_A_Car;
 
 /**
@@ -9,18 +5,53 @@ package Rent_A_Car;
  * @author David Gomez, Gabriel Brilla, Gerald Blanco, Alexander Torres
  */
 public class NodoC {
+    
+    /*
+    * Se declaran las variables que se van a usar
+    * en esta y las otras clases.
+    */    
+    
     private Cliente dato;
     private NodoC next;
+    
+    
+    //<editor-fold defaultstate="collapsed" desc="toString">
+    /*
+    * El metodo to string se usa para convertir 
+    * a "dato" que es tipo cliente a un string
+    * que podamos usar luego para listar 
+    * la lista completa.
+    */ 
+    //</editor-fold>
     
     @Override
     public String toString(){
         return dato.toString();
     }
     
+    //<editor-fold defaultstate="collapsed" desc="Override">
+    /*
+    * Se especifica que dato
+    * sera con una variable tipo
+    * Cliente llamada dato
+    * y se le asigna a este dato 
+    * el dato que se declaro al 
+    * inicio de la clase
+    */    
+    //</editor-fold>
+    
     public NodoC(Cliente dato){
         this.dato=dato;
     }
-
+    
+    //<editor-fold defaultstate="collapsed" desc="Setter y Getter">
+    /*
+    * Se crea un setter y un getter para todas
+    * las variables que se van a usar en las 
+    * otras clases y el main.
+    */
+    //</editor-fold>
+    
     public Cliente getDato() {
         return dato;
     }
